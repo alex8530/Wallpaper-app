350a1c3fcfb792011a0c2f65fd8f8d0583054224
alex <aliabuharb8530@gmail.com>
2018-11-25 23:48:12 +0200

    Add FullScreen theme ForAllActivites instead of java code

--- app/src/main/java/com/example/alex/wallpaperapp/MainActivity.java
+++ app/src/main/java/com/example/alex/wallpaperapp/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //this is for splash screen
