f5c670334c68fbf303462064c851e11f920b9311
alex <aliabuharb8530@gmail.com>
2018-11-25 15:05:44 +0200

    Fix Full Screen theme with replacing it with java code

--- app/src/main/java/com/example/alex/wallpaperapp/MainActivity.java
+++ app/src/main/java/com/example/alex/wallpaperapp/MainActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //this is for splash screen
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(getApplicationContext(),HomeActivity.class));
             }
-        },3000);
+        },5000);
 
 
     }
