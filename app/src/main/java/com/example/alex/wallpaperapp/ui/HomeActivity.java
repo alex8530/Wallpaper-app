fb1d2458bb7f9a1e2b9f580ec37598e598f5ac09
alex <aliabuharb8530@gmail.com>
2018-12-02 03:39:53 +0200

    Add SignInWithGoogle

--- app/src/main/java/com/example/alex/wallpaperapp/ui/HomeActivity.java
+++ app/src/main/java/com/example/alex/wallpaperapp/ui/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.example.alex.wallpaperapp.ui;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,58 +23,117 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.example.alex.wallpaperapp.R;
 import com.example.alex.wallpaperapp.adapter.MyFragmentAdapter;
 import com.example.alex.wallpaperapp.utils.Common;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.util.ExtraConstants;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.app.Activity.RESULT_OK;
+
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private static final String TAG = "HomeActivity";
     @BindView(R.id.viewpager)
     ViewPager mViewPager;
     @BindView(R.id.tabLayout)
     TabLayout mTabLayout;
 
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    public static final String IDP_RESPONSE = "extra_idp_response";
+
+    FirebaseUser currentUSer;
+    MyFragmentAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("ALEX WallPaper");
-        setSupportActionBar(toolbar);
+        currentUSer = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUSer == null) {
+            startActivity(AuthUiActivity.createIntent(this));
+            finish();
+            return;
+        }
 
+        setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
 
-//        //request runtime permission external storage
-//        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                !=PackageManager.PERMISSION_GRANTED)
-//        {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},Common.PERMISSION_REQUEST_CODE);
-//            }
-//        }
 
-        MyFragmentAdapter adapter= new MyFragmentAdapter(getSupportFragmentManager(),this);
+
+        toolbar.setTitle("ALEX WallPaper");
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        setUpActionBarDrawerToggle();
+
+        setUpTapLayout();
+
+        loadUserInfo();
+    }
+
+    private void setUpTapLayout() {
+          adapter= new MyFragmentAdapter(getSupportFragmentManager(),this);
         mViewPager.setAdapter(adapter);
-
         mTabLayout.setupWithViewPager(mViewPager);
+    }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    private void setUpActionBarDrawerToggle() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    private void loadUserInfo() {
+      if (currentUSer!=null){
+          View view=   navigationView.getHeaderView(0);
+          TextView txt_email = view.findViewById(R.id.nav_email);
+          txt_email.setText(currentUSer.getEmail());
+          String messamge="Welcome "+ currentUSer.getEmail() ;
+          Snackbar.make(drawer ,messamge,Snackbar.LENGTH_LONG).show();
+      }
+
+    }
+
+    @NonNull
+    public static Intent createIntent(@NonNull Context context, @Nullable IdpResponse response) {
+        return new Intent().setClass(context, HomeActivity.class)
+                .putExtra( IDP_RESPONSE, response);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: ");
+        if (requestCode==Common.SIGN_IN_GOOGLE_REQUEST_CODE){
+            if (resultCode==RESULT_OK){
+                //thats mean , Now the user sign in successfuly , so show messamge to welcome
+                String messamge="Welcome "+ currentUSer.getEmail().toString();
+                Snackbar.make(drawer ,messamge,Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -109,17 +173,8 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_photos) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
--- app/src/main/java/com/example/alex/wallpaperapp/utils/Common.java
+++ app/src/main/java/com/example/alex/wallpaperapp/utils/Common.java
@@ -8,6 +8,7 @@ import com.example.alex.wallpaperapp.model.WallPaperItem;
 public final class Common  {
     public static final String REF_CATEGORY_BACKGROUND="CategoryBackground";
     public static final String REF_WALLPAPER  = "wallPaper";
+    public static final int SIGN_IN_GOOGLE_REQUEST_CODE = 666;
     public static   String CATEGORY_SELECTED ;
     public static   String CATEGORY_SELECTED_ID ;
 
