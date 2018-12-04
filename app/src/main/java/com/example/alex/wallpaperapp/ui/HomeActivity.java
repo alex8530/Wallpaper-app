fc0c84fa64f358a644fcc534740762dc10b7a6f1
alex <aliabuharb8530@gmail.com>
2018-12-04 04:47:48 +0200

    Change navigationBottom menu

--- app/src/main/java/com/example/alex/wallpaperapp/ui/HomeActivity.java
+++ app/src/main/java/com/example/alex/wallpaperapp/ui/HomeActivity.java
@@ -208,20 +208,7 @@ public class HomeActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_aboutMe) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
