ddeda60ace9c4f5a78f9dd6034f682cf82e68e7e
alex <aliabuharb8530@gmail.com>
2018-12-08 22:19:07 +0200

    Add About Activity and implement it

--- app/src/main/java/com/example/alex/wallpaperapp/ui/HomeActivity.java
+++ app/src/main/java/com/example/alex/wallpaperapp/ui/HomeActivity.java
@@ -30,6 +30,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.example.alex.wallpaperapp.AboutActivity;
 import com.example.alex.wallpaperapp.R;
 import com.example.alex.wallpaperapp.adapter.MyFragmentAdapter;
 import com.example.alex.wallpaperapp.utils.Common;
@@ -228,6 +229,13 @@ public class HomeActivity extends AppCompatActivity
 
 
         }
+
+        if (id==R.id.nav_about){
+        startActivity(new Intent(this,AboutActivity.class));
+
+
+        }
+
 
         DrawerLayout drawer =findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
