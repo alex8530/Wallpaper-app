8c5dbc6208691c198429a8ff640e726a2efecb36
alex <aliabuharb8530@gmail.com>
2018-12-09 15:27:12 +0200

    Add Dialog Activity style

--- app/src/main/java/com/example/alex/wallpaperapp/ui/ViewUserImageActivity.java
+++ app/src/main/java/com/example/alex/wallpaperapp/ui/ViewUserImageActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -55,6 +57,16 @@ public class ViewUserImageActivity extends AppCompatActivity {
 
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        Toolbar toolbar =   findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        }
 
         loadImage();
 
@@ -141,4 +153,14 @@ public class ViewUserImageActivity extends AppCompatActivity {
         }
     };
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==android.R.id.home){
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
